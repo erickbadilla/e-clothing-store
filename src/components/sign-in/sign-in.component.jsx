01685172057e76df0a98bef7eeb505d487dc8423
Crytek01 <erickbadilla99@gmail.com>
2020-12-07 16:10:38 -0600

    added redux, and implemented userReducer and user Actions to header

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -60,7 +60,11 @@ class SignIn extends React.Component {
 
           <div className="buttons">
             <CustomButton type="submit">Sign In</CustomButton>
-            <CustomButton onClick={SignInWithGoogle} isGoogleSignIn>
+            <CustomButton
+              type="button"
+              onClick={SignInWithGoogle}
+              isGoogleSignIn
+            >
               Sign In With Google
             </CustomButton>
           </div>
