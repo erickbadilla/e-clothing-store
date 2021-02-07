9660b4959c860b9b4243ad29fa9569782152185c
Erick Badilla <erickbadilla@Ericks-iMac.local>
2021-02-07 15:13:32 -0600

    Fixed error in checkout component

--- client/src/pages/checkout/checkout.component.jsx
+++ client/src/pages/checkout/checkout.component.jsx
@@ -19,46 +19,50 @@ import {
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 import StripeCheckoutButton from "../../components/stripe-button/stripe.component";
 
-const CheckoutPage = ({ cartItems, total }) => (
-  <CheckoutPageContainer>
-    <CheckoutHeaderContainer>
-      <HeaderBlockContainer>
-        <span>Product</span>
-      </HeaderBlockContainer>
+const CheckoutPage = ({ cartItems, total }) => {
+  let [month, , year] = new Date().toLocaleString("en-US").split("/");
 
-      <HeaderBlockContainer>
-        <span>Description</span>
-      </HeaderBlockContainer>
+  return (
+    <CheckoutPageContainer>
+      <CheckoutHeaderContainer>
+        <HeaderBlockContainer>
+          <span>Product</span>
+        </HeaderBlockContainer>
 
-      <HeaderBlockContainer>
-        <span>Quantity</span>
-      </HeaderBlockContainer>
+        <HeaderBlockContainer>
+          <span>Description</span>
+        </HeaderBlockContainer>
 
-      <HeaderBlockContainer>
-        <span>Price</span>
-      </HeaderBlockContainer>
+        <HeaderBlockContainer>
+          <span>Quantity</span>
+        </HeaderBlockContainer>
 
-      <HeaderBlockContainer>
-        <span>Remove</span>
-      </HeaderBlockContainer>
-    </CheckoutHeaderContainer>
+        <HeaderBlockContainer>
+          <span>Price</span>
+        </HeaderBlockContainer>
 
-    {cartItems.map((cartItem) => (
-      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-    ))}
-    <TotalContainer>
-      <span>TOTAL: ${total}</span>
-    </TotalContainer>
+        <HeaderBlockContainer>
+          <span>Remove</span>
+        </HeaderBlockContainer>
+      </CheckoutHeaderContainer>
 
-    <TestWarningContainer>
-      *Please use the following test credit card for payments*
-      <br />
-      4242 4242 4242 4242 - Exp:{Date.prototype.getMonth()}/{Date.prototype.getYear()} - CVV:123
-    </TestWarningContainer>
-    
-    <StripeCheckoutButton price={total} />
-  </CheckoutPageContainer>
-);
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+      <TotalContainer>
+        <span>TOTAL: ${total}</span>
+      </TotalContainer>
+
+      <TestWarningContainer>
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp:{month}/{year.substring(2, 4)} - CVV:123
+      </TestWarningContainer>
+
+      <StripeCheckoutButton price={total} />
+    </CheckoutPageContainer>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
