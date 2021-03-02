32523949d8bfd376a7153661535752044690f881
Erick Badilla <erickbadilla@Ericks-iMac.local>
2021-03-01 23:54:37 -0600

    re uploading to heroku

--- client/src/pages/home/homepage.component.jsx
+++ client/src/pages/home/homepage.component.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { Profiler } from "react";
 import { HomePageContainer } from "./homepage.styles";
 import Directory from "../../components/directory/directory.component";
 
 const HomePage = () => (
   <HomePageContainer>
-    <Directory />
+    <Profiler
+      id="Directory"
+      onRender={(id, phase, actualDuration) => {
+        console.log({
+          id,
+          phase,
+          actualDuration,
+        });
+      }}
+    >
+      <Directory />
+    </Profiler>
   </HomePageContainer>
 );
 
 export default HomePage;
-   
