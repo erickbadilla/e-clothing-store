6cefe807a557983ad904f727c368bb46a81f12e5
Erick <39202274+Crytek01@users.noreply.github.com>
2021-04-24 14:33:46 -0600

    updated depracated bodyPaser to use node's parser

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const path = require("path");
 const enforce = require("express-sslify");
 const compression = require("compression");
@@ -11,20 +10,15 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression());
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(
-  bodyParser.urlencoded({
+  express.urlencoded({
     extended: true,
-  })
-);
-app.use(
-  enforce.HTTPS({
-    trustProtoHeader: true,
   })
 );
 
 if (process.env.NODE_ENV === "production") {
+  app.use(compression());
   app.use(
     enforce.HTTPS({
       trustProtoHeader: true,
