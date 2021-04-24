87819295625bae2e1ff7ce3d9c4925296938c5c7
Erick <39202274+Crytek01@users.noreply.github.com>
2021-04-24 13:18:56 -0600

    fixed service worker registration

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
 
 import { store, persistor } from "./redux/store";
-import * as serviceWorker from "./service-worker";
+import * as serviceWorker from "./serviceWorkerRegistration";
 
 ReactDOM.render(
   <Provider store={store}>
